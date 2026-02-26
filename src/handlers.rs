41c20950a365a26fececbd77aa2e11da7e7affa8
pycd01 <pycd01@vse.cz>
2026-02-26 21:05:33 +0100

    init with getall

---
+++ src/handlers.rs
@@ -0,0 +1,198 @@
+use axum::{extract, http};
+use axum::extract::path::ErrorKind::Message;
+use serde::{Deserialize, Serialize};
+use jsonwebtoken::{encode, decode, Header, Validation, EncodingKey, DecodingKey, errors::Error as JwtError};
+use sqlx::{FromRow, PgPool};
+use chrono::{Utc, Duration};
+use bcrypt::{verify, hash};
+use hyper::StatusCode;
+use sqlx::types::Text;
+const SECRET: &[u8] = b"";
+
+#[derive(Debug, Deserialize, Serialize, FromRow)]
+pub struct Articles {
+    id: i32,
+    name: String,
+    date: chrono::NaiveDate,
+    text: String
+}
+#[derive(Debug, Deserialize, Serialize, FromRow)]
+pub struct Cities {
+    id: i32,
+    name: String,
+    is_displayed: bool
+}
+#[derive(Debug, Deserialize, Serialize, FromRow)]
+pub struct Deals {
+    id: i32,
+    date: chrono::NaiveDate,
+    shopId: i32,
+    enddate: chrono::NaiveDate,
+    value: String,
+    text: String,
+    priority: i16
+}
+#[derive(Debug, Deserialize, Serialize, FromRow)]
+pub struct Photos {
+    id: i32,
+    desc: Option<String>,
+    name: String,
+    url: Option<String>
+}
+#[derive(Debug, Deserialize, Serialize, FromRow)]
+pub struct Shops {
+    id: i32,
+    name: String,
+    Mon: String,
+    Tue: String,
+    Wed: String,
+    Thu: String,
+    Fri: String,
+    Sat: String,
+    mapsrc: String,
+    adresa: String,
+    urlname: String
+}
+#[derive(Debug, Deserialize, Serialize, FromRow)]
+pub struct Users {
+    id: i64,
+    email: String,
+    phone: String,
+    cityid: i64,
+    date_registered: Option<chrono::DateTime<chrono::Utc>>,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+    exp: usize
+}
+
+impl Articles {
+    pub fn new(id: i32, name: String, date: chrono::NaiveDate, text: String) -> Articles {
+        Self { id, name, date, text }
+    }
+}
+impl Cities {
+    pub fn new(id: i32, name: String, is_displayed: bool) -> Cities {
+        Self { id, name, is_displayed }
+    }
+}
+impl Deals {
+    pub fn new(id: i32, date: chrono::NaiveDate, shopId: i32, enddate: chrono::NaiveDate, value: String, text: String, priority: i16) -> Deals {
+        Self { id, date, shopId, enddate, value, text, priority }
+    }
+}
+impl Photos {
+    pub fn new(id: i32, desc: Option<String>, name: String, url: Option<String>) -> Photos {
+        Self { id, desc, name, url }
+    }
+}
+impl Shops {
+    pub fn new(id: i32, name: String, Mon: String, Tue: String, Wed: String, Thu: String, Fri: String, Sat: String, mapsrc: String, adresa: String, urlname: String) -> Shops {
+        Self {id, name, Mon, Tue, Wed, Thu, Fri, Sat, mapsrc, adresa, urlname }
+    }
+}
+impl Users {
+    pub fn new(id: i64, email: String, phone: String, cityid: i64, date_registered: Option<chrono::DateTime<chrono::Utc>>) -> Users {
+        Self { id, email, phone, cityid, date_registered }
+    }
+}
+
+pub async fn hello_world() -> &'static str {
+    "hello world"
+}
+
+fn generate_jwt(username: &str) -> Result<String, JwtError> {
+    let expiration = Utc::now()
+        .checked_add_signed(Duration::hours(24))
+        .expect("valid timestamp")
+        .timestamp() as usize;
+
+    let claims = Claims {
+        sub: username.to_owned(),
+        exp: expiration,
+    };
+
+    encode(&Header::default(), &claims, &EncodingKey::from_secret(SECRET))
+}
+
+fn validate_jwt(token: &str) -> Result<Claims, JwtError> {
+    let token_data = decode::<Claims>(
+        token,
+        &DecodingKey::from_secret(SECRET),
+        &Validation::default(),
+    )?;
+    Ok(token_data.claims)
+}
+pub async fn get_articles(
+    extract::State(pool): extract::State<PgPool>
+) -> Result<axum::Json<Vec<Articles>>, String> {
+    let res = sqlx::query_as::<_, Articles>("SELECT * FROM \"Articles\"")
+        .fetch_all(&pool)
+        .await
+        .map(axum::Json)
+        .map_err(|e| e.to_string());
+
+    res
+}
+
+pub async fn get_cities(
+    extract::State(pool): extract::State<PgPool>
+) -> Result<axum::Json<Vec<Cities>>, String> {
+    let res = sqlx::query_as::<_, Cities>("SELECT * FROM \"Cities\"")
+        .fetch_all(&pool)
+        .await
+        .map(axum::Json)
+        .map_err(|e| e.to_string());
+
+    res
+}
+
+pub async fn get_deals(
+    extract::State(pool): extract::State<PgPool>
+) -> Result<axum::Json<Vec<Deals>>, String> {
+    let res = sqlx::query_as::<_, Deals>("SELECT * FROM \"Deals\"")
+        .fetch_all(&pool)
+        .await
+        .map(axum::Json)
+        .map_err(|e| e.to_string());
+
+    res
+}
+
+pub async fn get_photos(
+    extract::State(pool): extract::State<PgPool>
+) -> Result<axum::Json<Vec<Photos>>, String> {
+    let res = sqlx::query_as::<_, Photos>("SELECT * FROM \"Photos\"")
+        .fetch_all(&pool)
+        .await
+        .map(axum::Json)
+        .map_err(|e| e.to_string());
+
+    res
+}
+
+
+pub async fn get_shops(
+    extract::State(pool): extract::State<PgPool>
+) -> Result<axum::Json<Vec<Shops>>, String> {
+    let res = sqlx::query_as::<_, Shops>("SELECT * FROM \"Shops\"")
+        .fetch_all(&pool)
+        .await
+        .map(axum::Json)
+        .map_err(|e| e.to_string());
+
+    res
+}
+
+pub async fn get_users(
+    extract::State(pool): extract::State<PgPool>
+) -> Result<axum::Json<Vec<Users>>, String> {
+    let res = sqlx::query_as::<_, Users>("SELECT * FROM \"Users\"")
+        .fetch_all(&pool)
+        .await
+        .map(axum::Json)
+        .map_err(|e| e.to_string());
+
+    res
+}
---
+++ src/main.rs
@@ -0,0 +1,68 @@
+mod handlers;
+
+use std::collections::HashMap;
+use axum::routing::{Router, delete, get, post, put};
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+use std::sync::{Arc, Mutex};
+use dotenvy::dotenv;
+use sqlx::PgPool;
+use tower_http::cors::{Any, CorsLayer};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    dotenv().ok();
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+    let database_url = env::var("DATABASE_URL").expect("missing DATABASE_URL env");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
+
+    let state = AppState { pool };
+
+    let api_routes = Router::new()
+        .route("/", get(handlers::hello_world))
+        .route("/articles", get(handlers::get_articles))
+        .route("/cities", get(handlers::get_cities))
+        .route("/deals", get(handlers::get_deals))
+        .route("/photos", get(handlers::get_photos))
+        .route("/shops", get(handlers::get_shops))
+        //.route("/users", get(handlers::get_users))
+
+/*        .route("/users_conversations/{id}", get(handlers::get_users_conversations))
+        .route("/users/{id}", get(handlers::get_user_by_id))
+        .route("/users/name={username}", get(handlers::get_user_by_username))
+        .route("/send_message", post(handlers::send_message))
+        .route("/register_user", post(handlers::register_user))
+        .route("/login_user", post(handlers::login_user))
+        .route("/create_conversation", post(handlers::create_conversation))
+
+        .route("/get_users_debug", get(handlers::get_users_DEBUG))
+        .route("/get_conversations_debug", get(handlers::get_conversations_DEBUG))
+        .route("/get_conversationsusers_debug", get(handlers::get_conversationsusers_DEBUG))
+        .route("/get_friends_debug", get(handlers::get_friends_DEBUG))
+        .route("/get_friendrequests_debug", get(handlers::get_friendrequests_DEBUG))
+        .route("/get_messages_debug", get(handlers::get_messages_DEBUG))
+*/
+        .layer(cors)
+        .with_state(state.pool);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, api_routes)
+        .await?;
+
+    Ok(())
+}
+
+#[derive(Clone)]
+struct AppState {
+    pool: PgPool
+}
